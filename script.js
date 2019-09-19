d00e21b56a4fb3f681365740f140dcb2db47e53c
Ubuntu <ubuntu@ip-172-31-23-235.ap-southeast-1.compute.internal>
2019-09-19 06:47:04 +0000

    Using annoyomous functions

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 /* global $ */
-function xyz()
-{
-    let s = {
+
+// DOM READY
+$(function(){
+  
+  $("#change").click(function(){
+    $("#greeting img").attr('src', 'german.jpg');
+  })
+  
+  $("#expand").click(function(){
+     let s = {
       'animation-name': 'sizechange',
       'animation-duration': '4s',
       'animation-fill-mode': 'forwards'
     }
     
     $("#latest-arrivals .product-item").css(s);
-}
-
-function changeImage()
-{
-  $('#greeting img').attr('src', 'german.jpg');
-}
-
-// DOM READY
-$(function(){
-  $("#change").click(function(){
-    $("#greeting img").attr('src', 'german.jpg');
   })
 })
