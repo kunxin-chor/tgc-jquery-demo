f65d69e15b7980c7d43f1f49e8d84a4b705424fd
Ubuntu <ubuntu@ip-172-31-23-235.ap-southeast-1.compute.internal>
2019-09-19 03:34:11 +0000

    Added a button that will play an animation

---
+++ script.js
@@ -0,0 +1,11 @@
+/* global $ */
+function xyz()
+{
+    let s = {
+      'animation-name': 'sizechange',
+      'animation-duration': '4s',
+      'animation-fill-mode': 'forwards'
+    }
+    
+    $("#latest-arrivals").css(s);
+}
